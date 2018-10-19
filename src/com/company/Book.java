2bcc5d82d56e3a7bdaab9bb9558499b8803c3b00
Ermiji <eghebrez@gmail.com>
2018-10-18 21:24:50 -0400

    Completed app

--- src/com/company/Book.java
+++ src/com/company/Book.java
@@ -20,52 +20,27 @@ public class Book extends Author {
     }
 
     public String getTitle() {
+        return title;
+    }
 
-        private Author author;
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-    public Book() {
+    public String getIsbn() {
+        return isbn;
+    }
 
-        }
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
 
-    public Book(String title, String isbn, Author author) {
-            this.title = title;
-            this.isbn = isbn;
-            this.author = author;
-        }
+    public ArrayList<Author> getAuthors() {
+        return authors;
+    }
 
-        public String getTitle () {
-            return title;
-        }
-
-        public void setTitle (String title){
-            this.title = title;
-        }
-
-        public String getIsbn () {
-            return isbn;
-        }
-
-        public void setIsbn (String isbn){
-
-            this.isbn = isbn;
-        }
-
-        public ArrayList<Author> getAuthors () {
-            return authors;
-        }
-
-        public void setAuthors (ArrayList < Author > authors) {
-            this.authors = authors;
-
-            this.isbn = isbn;
-        }
-
-        public Author getAuthor () {
-            return author;
-        }
-
-        public void setAuthor (Author author){
-            this.author = author;
-        }
+    public void setAuthors(ArrayList<Author> authors) {
+        this.authors = authors;
     }
 }
+
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,12 @@
 package com.company;
-import java.lang.reflect.Array;
+
 import java.util.ArrayList;
 import java.util.Scanner;
-
 public class Main {
-
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
         ArrayList<Book> books = new ArrayList<Book>();
         ArrayList<Author> authors = new ArrayList<Author>();
-
 
         String responseBook = "n";
         String responseAuthor = "n";
@@ -19,110 +15,65 @@ public class Main {
         do {
             System.out.println("Do you want to enter a book information? ");
             responseBook = scan.nextLine();
-            Book b = new Book();
-            Author au= new Author();
+            //  Book b = new Book();
+            Author au = new Author();
             if (responseBook.equalsIgnoreCase("y")) {
 
-        ArrayList<Book> books = new ArrayList<>();
+
+                    Book b = new Book();
+                    System.out.print("Enter the title: ");
+                    b.setTitle(scan.nextLine());
+                    System.out.print("Enter the ISBN: ");
+                    b.setIsbn(scan.nextLine());
+
+                    do {
+                        System.out.println("Do you want to enter an author's information? ");
+                        responseAuthor = scan.nextLine();
+                        Author p = new Author();
+
+                        if (responseAuthor.equalsIgnoreCase("y")) {
+
+                            System.out.print("Enter the Authors First Name: ");
+                            p.setfName(scan.nextLine());
+                            System.out.print("Enter the Authors Last Name: ");
+                            p.setlName(scan.nextLine());
+                            System.out.print("Enter the authors Email Address: ");
+                            p.seteAddress(scan.nextLine());
+                            System.out.print("Enter the Authors Phone number: ");
+                            p.setPhoneNumber(scan.nextLine());
+
+                            b.getAuthors().add(p);
+                            // authors.add(p);
+                        }
+
+                    } while (responseAuthor.equalsIgnoreCase("y"));
+
+                    //b.setAuthors(authors);
+                    books.add(b);
 
 
-        String response = "n";
-
-
-        int choice = 0;
-
-        do {
-            System.out.println("Do you want to enter a book information? ");
-            response = scan.nextLine();
-
-            if (response.equalsIgnoreCase("y")) {
-
-                Book b = new Book();
-                System.out.print("Enter the title: ");
-                b.setTitle(scan.nextLine());
-                System.out.print("Enter the ISBN: ");
-                b.setIsbn(scan.nextLine());
-
-                do {
-                    System.out.println("Do you want to enter an author's information? ");
-                    responseAuthor = scan.nextLine();
-                    Author p = new Author();
-
-                    if (responseAuthor.equalsIgnoreCase("y")) {
-
-                        System.out.print("Enter the Authors First Name: ");
-                        p.setfName(scan.nextLine());
-                        System.out.print("Enter the Authors Last Name: ");
-                        p.setlName(scan.nextLine());
-                        System.out.print("Enter the authors Email Address: ");
-                        p.seteAddress(scan.nextLine());
-                        System.out.print("Enter the Authors Phone number: ");
-                        p.setPhoneNumber(scan.nextLine());
-
-                        b.getAuthors().add(p);
-                       // authors.add(p);
-                    }
-
-                } while (responseAuthor.equalsIgnoreCase("y"));
-
-                //b.setAuthors(authors);
-                books.add(b);
+                }
 
 
             }
+            while (responseBook.equalsIgnoreCase("y")) ;
 
 
+            System.out.println("books.size(): " + books.size());
+            for (Book bk : books) {
+                System.out.println(bk.getTitle());
+                System.out.print("");
+                System.out.println(bk.getIsbn());
+                System.out.print("");
+                System.out.println("Authors: " + bk.getAuthors().size());
 
-        }while (responseBook.equalsIgnoreCase("y"));
+                for (Author auther : bk.getAuthors()) {
+                    System.out.println((auther.toString()));
+                }
 
-        System.out.println("books.size(): " + books.size());
-        for (Book bk : books) {
-            System.out.println(bk.getTitle());
-            System.out.print("");
-            System.out.println(bk.getIsbn());
-            System.out.print("");
-            System.out.println("Authors: " + bk.getAuthors().size());
-
-            for(Author auther: bk.getAuthors()) {
-                System.out.println((auther.toString()));
             }
+
 
         }
 
-
     }
-
-}
-
-
-                Author p = new Author();
-                System.out.print("Enter the Authors First Name: ");
-                p.setfName(scan.nextLine());
-                System.out.print("Enter the Authors Last Name: ");
-                p.setlName(scan.nextLine());
-                System.out.print("Enter the authors Email Address: ");
-                p.seteAddress(scan.nextLine());
-                System.out.print("Enter the Authors Phone number: ");
-                p.setPhoneNumber(scan.nextLine());
-
-                b.setAuthor(p);
-                System.out.println(b.getTitle());
-                System.out.println(b.getIsbn());
-                System.out.println(b.getAuthor().getfName());
-                System.out.println(b.getAuthor().getlName());
-                System.out.println(b.getAuthor().geteAddress());
-                System.out.println(b.getAuthor().getPhoneNumber());
-
-                books.add(b);
-            }
-        } while (response.equalsIgnoreCase("y"));
-
-        for (Book bk : books) {
-            if () {
-                System.out.print(bk.getTitle());
-                System.out.print(" ");
-                System.out.print((bk.getAuthor()));
-            }
-        }
-    }
-}
